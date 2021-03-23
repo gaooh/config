50dd789d2b807763d5de138dbef4a68b8d0481a6
gaooh <asami@viibar.com>
2021-03-23 09:29:13 +0900

    init

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
-set XDG_CONFIG_HOME $HOME/.config 
+set XDG_CONFIG_HOME $HOME/.config
 set PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
 set PATH /opt/homebrew/opt/postgresql@11/bin $PATH
+set PATH $HOME/.nodebrew/current/bin $PATH
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
